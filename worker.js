5abb453c2be20dfce3a5f248e9483481e6ac20c8
Nathan Clevenger <nateclev@gmail.com>
2022-09-30 11:05:49 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-import { json } from 'https://raw.githubusercontent.com/drivly/apis.do/main/apis.js'
+import apis from 'https://raw.githubusercontent.com/drivly/apis.do/main/apis.js'
 import { json } from 'http://pkg.do/itty-router-extras'
 
 export default {
